45850990c4570c347a0c4351f95f4e8c17689177
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2023-10-26 10:04:01 -0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { Route, Routes, useLocation } from 'react-router-dom'
 import './App.css'
+import Landing from './components/Landing'
+import Home from './components/Home'
+import Favorites from './components/Favorites'
+import Detail from './components/Detail'
+import NavBar from './components/NavBar'
 
 function App() {
-	//* Comentario de prueba
+	
+	// console.log(useLocation().pathname);
+	const { pathname } = useLocation();
+
   return (
 		<div>
-			<h1>Repaso Módulo 2</h1>
-			<hr />
-			<h2>Cohorte FT44a</h2>
-			<h2>Rick & Morty - Episodes</h2>
-			<h3>Ver README.md para ver detalles del proyecto</h3>
+			{ pathname !== "/" ? <NavBar /> : null }
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/favorites" element={<Favorites />} />
+				<Route path="/detail/:id" element={<Detail />} />
+				<Route path="*" element={<Landing />} />
+			</Routes>
 		</div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar(props) {
   return (
     <div>
-      <h2>NavBar</h2>
+      <span>Rick&Morty Episodes </span>
+      <button>
+        <NavLink
+          to="/home"
+          style={({ isActive }) => isActive ? { color: "coral"} : { color: "grey" } }
+        >Home</NavLink>
+      </button>
+      <button>
+        <NavLink
+          to="/favorites"
+          style={({ isActive }) => isActive ? { color: "coral"} : { color: "grey" } }
+        >Favorites</NavLink>
+      </button>
     </div>
   );
 }
